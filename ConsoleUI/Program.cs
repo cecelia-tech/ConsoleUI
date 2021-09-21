4487ca499e84edaa90d3aca049ce92200f098943
Vita <vitagriciute@gmail.com>
2021-09-22 00:01:51 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+// Dependency injection, Serilog, Settings
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+
+        }
+    }
+}
